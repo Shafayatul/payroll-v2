33df48692fc39e430d7ae36222b3a61164cf5427
SajalKundu <sajalkundu098@gmail.com>
2019-12-31 15:59:51 +0600

    Laravel Localization ext add complete

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(
+[
+	'prefix' => LaravelLocalization::setLocale(),
+	'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+], function(){
+	Route::get('/', function () {
+	    return view('auth.login');
+	});
 
-Route::get('/', function () {
-    return view('auth.login');
+	Auth::routes();
+
+	Route::group(['middleware' => 'auth'], function(){
+		Route::get('/home', 'HomeController@index')->name('home');
+		Route::get('/password-change', 'UsersController@passwordWordChange')->name('password-change');
+		Route::post('/password-update', 'UsersController@passwordUpdate')->name('password-update');
+		Route::resource('users', 'UsersController');
+		Route::get('/user-inactive/{id}', 'UsersController@userInactive')->name('user-inactive');
+		Route::get('/user-active/{id}', 'UsersController@userActive')->name('user-active');
+		Route::resource('companies', 'CompaniesController');
+		Route::resource('industries', 'IndustriesController');
+		Route::resource('offices', 'OfficesController');
+	});
 });
 
-Auth::routes();
-
-Route::group(['middleware' => 'auth'], function(){
-	Route::get('/home', 'HomeController@index')->name('home');
-	Route::get('/password-change', 'UsersController@passwordWordChange')->name('password-change');
-	Route::post('/password-update', 'UsersController@passwordUpdate')->name('password-update');
-	Route::resource('users', 'UsersController');
-	Route::get('/user-inactive/{id}', 'UsersController@userInactive')->name('user-inactive');
-	Route::get('/user-active/{id}', 'UsersController@userActive')->name('user-active');
-	Route::resource('companies', 'CompaniesController');
-	Route::resource('industries', 'IndustriesController');
-	Route::resource('offices', 'OfficesController');
-});
