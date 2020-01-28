064bd453a9ed5fa9fd80537ef6912d891a0a375b
gammer42 <salmanhiderb@gmail.com>
2020-01-28 12:26:17 +0600

    Migration relation update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,14 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+
+use App\User;
+use App\Office;
+use App\Company;
+use App\Industry;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +21,36 @@ use Illuminate\Support\Str;
 |
 */
 
+$factory->define(Industry::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+    ];
+});
+
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('12345678'), // password
+        'remember_token' => Str::random(10),
+    ];
+});
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => Hash::make('12345678'), // password
+        'remember_token' => Str::random(10),
+    ];
+});
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => Hash::make('12345678'), // password
         'remember_token' => Str::random(10),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Company;
+use App\Industry;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Industry::class, 10)
+            ->create()
+            ->each(function ($companies) {
+                $companies->companies()->factory(Company::class, 5)
+                ->create()
+                ->each(function ($offices){
+                    $offices->offices()->factory(Offices::class, 7)->create();
+                });
+            });
     }
 }
